60d62da75e66781ecf32e258d97c8716a1448820
Ariel Yanarico <Ariel.Yanarico@tutanota.com>
2020-11-25 03:20:22 -0400

    Added support for public or private posts

--- src/models/post.js
+++ src/models/post.js
@@ -6,6 +6,10 @@ const postSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    isPublic: {
+      type: Boolean,
+      required: true,
+    },
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   },
   { timestamps: true },
